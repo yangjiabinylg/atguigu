f5e162bef11ad7547289cf7a44a8fc9f8b390835
yib1091947832 <1091947832@qq.com>
2019-10-12 15:52:56 +0800

    第3章SpringBoot日志   完成

--- src/main/java/com/unicom/atguigu/controller/HelloController.java
+++ src/main/java/com/unicom/atguigu/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.unicom.atguigu.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,11 +14,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 
 @Controller
+@Slf4j
 public class HelloController {
 
     @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
+        //日志级别
+        //由高到低   trace<debug<info<warn<error
+        //可以调整日志输出级别
+        log.trace("trace....");
+        log.debug("debug....");
+        log.info("info....");
+        log.warn("warn....");
+        log.error("error....");
         return "hello world";
     }
 
