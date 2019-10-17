b07fc56c0375ec3200d5dccd7f08d526c06eb150
yib1091947832 <1091947832@qq.com>
2019-10-17 18:23:33 +0800

    P61、尚硅谷_SpringBoot_数据访问-JDBC&自动配置原理

--- src/main/java/com/unicom/atguigu/controller/HelloController.java
+++ src/main/java/com/unicom/atguigu/controller/HelloController.java
@@ -1,8 +1,14 @@
 package com.unicom.atguigu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Copyright: Unicom (Zhejiang) Industrial Internet Co., Ltd.    2019 <br/>
@@ -19,6 +25,19 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @ResponseBody
+    @GetMapping("/query")
+    public Map<String,Object> map(){
+        List<Map<String, Object>> myList =
+                jdbcTemplate.queryForList("select * from department");
+
+        return myList.get(0);
     }
 
 
--- src/test/java/com/unicom/atguigu/AtguiguApplicationTests.java
+++ src/test/java/com/unicom/atguigu/AtguiguApplicationTests.java
@@ -2,15 +2,38 @@ package com.unicom.atguigu;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AtguiguApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws SQLException {
+
+        System.out.println(dataSource.getClass());
+
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+
+        /**
+              1.class com.zaxxer.hikari.HikariDataSource
+              2.HikariProxyConnection@1811975204 wrapping com.mysql.cj.jdbc.ConnectionImpl@216e0771
+              这是使用默认的jdbc的连接打印的数据
+         */
+
+
+
     }
 
 }
