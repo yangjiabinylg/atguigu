44d61aa6468ba8604f9a5e8e6db590432a7ff77d
yib1091947832 <1091947832@qq.com>
2019-10-18 16:06:24 +0800

    P71、尚硅谷_SpringBoot_原理-自定义starter

--- src/main/java/com/unicom/atguigu/controller/HelloController.java
+++ src/main/java/com/unicom/atguigu/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.unicom.atguigu.controller;
 
+import com.unicom.starter.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,11 +17,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+    @Autowired
+    HelloService helloService;
+
+
     @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world1111   "+helloService.sayHelloUnicom("   杨加彬  ");
     }
 
+    /**
+     *     需要自定义的stater在 zip里面（spring-boot-08-starter.zip）
+     */
 
 }
